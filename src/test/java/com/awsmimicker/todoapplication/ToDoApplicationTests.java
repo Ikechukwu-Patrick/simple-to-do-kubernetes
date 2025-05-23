af37b36fe5baf31dcd1d6506b55fa531b5d22f49
Ikechukwu-Patrick <patrickikechukwukalukalu@gmail.com>
2025-05-23 11:25:01 +0100

    Deploy Java TodoApp to ECS using LocalStack

--- src/test/java/com/awsmimicker/todoapplication/ToDoApplicationTests.java
+++ src/test/java/com/awsmimicker/todoapplication/ToDoApplicationTests.java
@@ -2,12 +2,13 @@ package com.awsmimicker.todoapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Add this
 class ToDoApplicationTests {
-
     @Test
     void contextLoads() {
+        // Test passes if context loads
     }
-
 }
